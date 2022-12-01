4e1e5ca6d95f66c95bb2775eee24b30f81417e45
Rafael Spíndola <rafa.spin95@gmail.com>
2022-12-01 13:25:14 -0300

    Rendered a single post page

--- src/main/java/com/spring/Blog/controller/BlogController.java
+++ src/main/java/com/spring/Blog/controller/BlogController.java
@@ -5,6 +5,7 @@ import com.spring.Blog.service.BlogService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -15,11 +16,19 @@ public class BlogController {
     @Autowired
     BlogService blogService;
 
-    @GetMapping(path = "/posts")
+    @GetMapping(path = "/posts") // selects/searches all posts
     public ModelAndView getPosts () {
         ModelAndView mv = new ModelAndView("posts");
         List<Post> posts = blogService.findAll();
         mv.addObject("posts", posts);
+        return mv;
+    }
+
+    @GetMapping(path = "/posts/{id}") // selects/searches a single post using an id
+    public ModelAndView getPostDetails (@PathVariable("id") Long id) {
+        ModelAndView mv = new ModelAndView("postDetails");
+        Post post = blogService.findById(id);
+        mv.addObject("post", post);
         return mv;
     }
 }
