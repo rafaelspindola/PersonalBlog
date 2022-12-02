0d55eac63a9524963f038760154263cbee76780b
Rafael Spíndola <rafa.spin95@gmail.com>
2022-12-01 23:03:33 -0300

    Added new post button, a get post form method and a new post method to save new posts

--- src/main/java/com/spring/Blog/controller/BlogController.java
+++ src/main/java/com/spring/Blog/controller/BlogController.java
@@ -4,11 +4,16 @@ import com.spring.Blog.model.Post;
 import com.spring.Blog.service.BlogService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.validation.Valid;
+import java.time.LocalDate;
 import java.util.List;
 @Controller
 @RequestMapping
@@ -30,5 +35,18 @@ public class BlogController {
         Post post = blogService.findById(id);
         mv.addObject("post", post);
         return mv;
+    }
+    @GetMapping(path = "/newpost")
+    public String getPostForm() {
+        return "postForm";
+    }
+    @PostMapping(path = "newpost")
+    public String savePost(@Valid Post post, BindingResult result, RedirectAttributes attributes) { // validates input from user
+        if (result.hasErrors()) {
+            return "redirect:/newpost";
+        }
+        post.setDate(LocalDate.now());
+        blogService.save(post);
+        return "redirect:/posts";
     }
 }
